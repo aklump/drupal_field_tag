5768b152e826aaf343b84859016f07c631536dcd
Aaron Klump <sourcecode@intheloftstudios.com>
2019-10-26 19:51:12 -0700

    update api docs

--- field_tag.api.php
+++ field_tag.api.php
@@ -5,12 +5,20 @@
  * Defines the API functions provided by the field_tag module.
  */
 
-// Get the tag value for a field.
-$tag = FieldTag::loadFromParent($entity, 'field_foo');
+// Load the tag value for a field.
+$field_tag = FieldTag::loadFromParent($entity, 'field_foo');
+$value = $field_tag->getTag();
+
+// Load the tag value for a field.
+$value = (string) FieldTag::loadFromParent($entity, 'field_foo');
 
 // Get the tags for a multi-value field.
 $tags = [
-  FieldTag::loadFromParent($entity, 'field_foo', 0),
-  FieldTag::loadFromParent($entity, 'field_foo', 1),
-  FieldTag::loadFromParent($entity, 'field_foo', 2),
+  (string) FieldTag::loadFromParent($entity, 'field_foo', 0),
+  (string) FieldTag::loadFromParent($entity, 'field_foo', 1),
+  (string) FieldTag::loadFromParent($entity, 'field_foo', 2),
 ];
+
+// To programmatically tag a field, then save the entity.
+$node->get('field_images')->get(0)->field_tag = 'foo bar baz';
+$node->save();
