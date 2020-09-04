17f5483a93ac55fa27e6fd3a6fe04788a95c6356
Aaron Klump <sourcecode@intheloftstudios.com>
2020-09-03 17:34:33 -0700

    Add field_tag_add migration process plugin.

--- documentation/source/_overview.md
+++ documentation/source/_overview.md
@@ -13,6 +13,7 @@ The use case for which this was written is this.  Allow the tagging of images on
 * Tags are not case-sensitive.
 * Tags may contain inner spaces, but neither leading nor trailing spaces nor commas.
 * Multiple tags must separated by a comma or "comma+space", e.g. 'foo,bar' or 'foo, bar'.
+* When tags are saved, duplicates will be removed and spaces will be added after each comma if they were not already present.
 
 ## Configuration
 
@@ -29,7 +30,7 @@ The use case for which this was written is this.  Allow the tagging of images on
 
 ## Tagging During Migration
 
-Here's an example of how you might tag an image field during a migration, this assumes field_images is already set up with field tagging.
+Here's an example of how you might tag an image field during a migration, this assumes field_images is already set up with field tagging and that 1) your source has no field tags or 2) you wish to replace those existing field tags.
 
     process:
       field_images:
@@ -45,6 +46,18 @@ Here's an example of how you might tag an image field during a migration, this a
             plugin: default_value
             default_value: hero
 
+And here is a migration where source has field tags and you wish to merge in new ones.
+    
+    process:
+        # First copy over as is from source.
+        field_images: field_images
+        
+        # Then add two tags to the first element only.
+        field_images/0/field_tag:
+          - plugin: field_tag_add
+            source: field_images/0/field_tag
+            field_tag: cover, card
+          
 ## Contributing
 
 If you find this project useful... please consider [making a donation](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4E5KZHDQCEUV8&item_name=Gratitude%20for%20aklump%2Ffield_tag).
---
+++ src/Plugin/migrate/process/FieldTagAdd.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\field_tag\Plugin\migrate\process;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\field_tag\FieldTagService;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Add field tags..
+ *
+ * Available configuration keys:
+ * - source: Source property.
+ * - field_tag: A CSV string of new tags to add to existing.
+ *
+ * The field_tag_add plugin returns the value of existing field tags, plus the
+ * new ones you are adding as a CSV string, normalized and deduped.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "field_tag_add"
+ * )
+ *
+ * Examples:
+ *
+ * This example will add the two tags, "cover" and "card" to the field tags
+ *   that already exist, if any.  Duplicates are automatically removed, so this
+ *   is really all you need as a plugin.
+ *
+ * @code
+ * process:
+ *   field_images/0/field_tag:
+ *     - plugin: field_tag_add
+ *       source: field_images/0/field_tag
+ *       field_tag: cover, card
+ * @endcode
+ */
+class FieldTagAdd extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\field_tag\FieldTagService
+   */
+  protected $fieldTagService;
+
+  /**
+   * FieldTagAdd constructor.
+   *
+   * @param \Drupal\field_tag\FieldTagService $field_tag_service
+   *   A service instance.
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, FieldTagService $field_tag_service) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->fieldTagService = $field_tag_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('field_tag'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (empty($this->configuration['field_tag'])) {
+      throw new MigrateException('Missing configuration key "field_tag"');
+    }
+
+    return $this->fieldTagService
+      ->normalizeFieldTagValue($value . ',' . $this->configuration['field_tag']);
+  }
+
+}
