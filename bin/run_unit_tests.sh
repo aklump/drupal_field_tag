95d956641e5af0f214cb6c3beeca7fb4fc2c7bac
Aaron Klump <sourcecode@intheloftstudios.com>
2024-05-22 16:22:44 -0700

    Rearrange unit test location; remove manual test requirement, add multiple php unit tests.

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -4,7 +4,7 @@ s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd 
 cd "$__DIR__/.."
 
 # https://phpunit.readthedocs.io/en/9.5/textui.html#command-line-options
-./vendor/bin/phpunit -c phpunit.xml "$@"
-#./vendor/bin/phpunit -c phpunit.xml --testdox "$@"
-#export XDEBUG_MODE=coverage;./vendor/bin/phpunit -c phpunit.xml "$@" --coverage-html=./reports
+./vendor/bin/phpunit -c tests/Unit/phpunit.xml "$@"
+#./vendor/bin/phpunit -c tests/Unit/phpunit.xml --testdox "$@"
+#export XDEBUG_MODE=coverage;./vendor/bin/phpunit -c tests/Unit/phpunit.xml "$@" --coverage-html=./reports
 #echo ./reports/index.html
